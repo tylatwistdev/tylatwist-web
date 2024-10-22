fd8a218df73af448087adbbfab9a8abac196cbae
tylatwistdev <official.tylatwist@gmail.com>
2024-10-22 16:27:24 +0530

    more update

--- src/Home/components/TestimonialsSection.js
+++ src/Home/components/TestimonialsSection.js
@@ -1,6 +1,5 @@
 // Testimonials.js
 
-
 import { TestimonialsData } from '../../data';
 
 import React from 'react';
@@ -11,7 +10,7 @@ import 'swiper/css/navigation';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 
 const TestimonialsSection = () => {
-    const testimonials = TestimonialsData()
+    const testimonials = TestimonialsData();
 
     return (
         <div className="container-fluid my-5">
@@ -19,37 +18,41 @@ const TestimonialsSection = () => {
             <Swiper
                 modules={[Pagination, Navigation, Autoplay]}
                 spaceBetween={30}
-                slidesPerView={4} // Show 4 testimonials at a time
-                loop={true}
+                slidesPerView={1} // Default for mobile
+                // loop={true}
                 pagination={{ clickable: true }}
                 navigation={true}
                 autoplay={{
                     delay: 5000,
                     disableOnInteraction: false,
                 }}
+                speed={2000}
                 breakpoints={{
-                    640: {
-                        slidesPerView: 1, // 1 slide at a time for mobile
+                    640: { // For mobile devices
+                        slidesPerView: 1, // 1 slide at a time
                     },
-                    768: {
-                        slidesPerView: 2, // 2 slides at a time for tablets
+                    768: { // For tablets
+                        slidesPerView: 2, // 2 slides at a time
                     },
-                    1024: {
-                        slidesPerView: 4, // 4 slides at a time for desktops
+                    1024: { // For desktops
+                        slidesPerView: 4, // 4 slides at a time
                     },
                 }}
+                style={{ height: '100%' }} // Set height to 100% of the swiper container
             >
-                {testimonials.map((testimonial) => (
-                    <SwiperSlide key={testimonial.id}>
-                        <div className="testimonial-card testimonial-image text-center p-4">
-                            <img src={testimonial.image} alt={testimonial.name} className="client-image rounded-circle mb-3" />
+                {testimonials.map((testimonial, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="testimonial-card text-center p-4">
+                            <img
+                                src={testimonial.image}
+                                alt={`Client ${testimonial.name}`}
+                                className="client-image rounded-circle mb-3"
+                                style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                            />
                             <h5 className="client-name">{testimonial.name}</h5>
-                            <div className="client-rating">
-                                {Array.from({ length: testimonial.rating }, (_, i) => (
-                                    <span key={i} className="star">&#9733;</span> // Star character
-                                ))}
-                            </div>
-                            <p className="client-feedback">{testimonial.feedback}</p>
+                            <p className="client-feedback">
+                                {testimonial.feedback}
+                            </p>
                         </div>
                     </SwiperSlide>
                 ))}
